6b724747409813abc5affefe6b8ef6ef332e6b74
NegatioN <joakimrishaug@gmail.com>
2018-10-30 21:11:45 +0100

    Let program write a file out instead of print

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filepath", default='resources/testfile.txt', help="Path of file to read from")
+parser.add_argument("-o", "--outpath", default='questions.txt', help="Path of question file")
 options = parser.parse_args()
 
 with open(options.filepath, 'r', encoding='utf-8') as f:
     sentences = f.readlines()
 
+
 rgx = re.compile('\s')
-for x in sentences:
-    arr = rgx.split(x.replace('\n', ''))
-    shuffle(arr)
-    print(' '.join(arr))
+with open(options.outpath, 'w+', encoding='utf-8') as o:
+    for x in sentences:
+        arr = rgx.split(x.replace('\n', ''))
+        shuffle(arr)
+        o.write(' '.join(arr) + '\n')
